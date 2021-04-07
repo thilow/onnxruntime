352f3c5299c6421e3ca11c6697048c65c69dde44
Guoyu Wang <wanggy@outlook.com>
2021-04-07 14:54:11 -0700

    test

--- tools/ci_build/github/android/build_aar_and_copy_artifacts.sh
+++ tools/ci_build/github/android/build_aar_and_copy_artifacts.sh
@@ -16,3 +16,9 @@ python3 /onnxruntime_src/tools/ci_build/github/android/build_aar_package.py \
     /onnxruntime_src/tools/ci_build/github/android/default_mobile_aar_build_settings.json
 
 # Copy the built artifacts to give folder for publishing
+PACKAGE_NAME=onnxruntime-mobile
+BASE_PATH=/build/aar_out/${BUILD_CONFIG}/com/microsoft/onnxruntime/${PACKAGE_NAME}/${ORT_VERSION}
+cp BASE_PATH/${PACKAGE_NAME}-${ORT_VERSION}-javadoc.jar  /home/onnxruntimedev/.artifacts
+cp BASE_PATH/${PACKAGE_NAME}-${ORT_VERSION}-sources.jar  /home/onnxruntimedev/.artifacts
+cp BASE_PATH/${PACKAGE_NAME}-${ORT_VERSION}.aar          /home/onnxruntimedev/.artifacts
+cp BASE_PATH/${PACKAGE_NAME}-${ORT_VERSION}.pom          /home/onnxruntimedev/.artifacts
